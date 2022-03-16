6e9df20ce97ab1cca6da35121f8aa7468e5fe6aa
NjustJiaweihan <1390445546@qq.com>
2022-03-16 22:46:57 +0800

    项目结构调整完成

--- libra-c-api/src/main/java/com/dream/libra/c/api/api/QuestionApi.java
+++ libra-c-api/src/main/java/com/dream/libra/c/api/api/QuestionApi.java
@@ -1,12 +1,27 @@
 package com.dream.libra.c.api.api;
 
+import com.dream.libra.response.Response;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 
 @RequestMapping("/cache")
 public interface QuestionApi {
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "query", value = "query", required = true, dataType = "QuestionQuery", paramType = "body")
+    })
     @PostMapping(value = "/question")
-    void get();
+    Response<Integer> get(@RequestBody Integer query);
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "query", value = "query", required = true, dataType = "QuestionListQuery", paramType = "body")
+    })
+    @PostMapping(value = "/questions")
+    Response<List<Integer>> list(@RequestBody Integer query);
 }
--- libra-c-application/src/main/java/com/dream/libra/c/application/controller/QuestionController.java
+++ libra-c-application/src/main/java/com/dream/libra/c/application/controller/QuestionController.java
@@ -1,13 +1,21 @@
 package com.dream.libra.c.application.controller;
 
 import com.dream.libra.c.api.api.QuestionApi;
+import com.dream.libra.response.Response;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class QuestionController implements QuestionApi {
 
     @Override
-    public void get() {
+    public Response<Integer> get(Integer query) {
+        return null;
+    }
 
+    @Override
+    public Response<List<Integer>> list(Integer query) {
+        return null;
     }
 }
